7fbee969f69fb9269e442d9156e43410dcbe031a
DiegoEstela <die.estela@gmail.com>
2022-09-01 20:41:17 -0300

    readByid

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ admin.initializeApp({
 const db = admin.firestore()
 const user = db.collection("users")
 
-const crearUsuario = async() =>{
+
+// CREATE A NEW USER
+const createUser = async() =>{
   const doc = user.doc()
   const result =  await doc.create({name: "ezequiel", age: 30})
   console.log(result)
 
 }
 
-crearUsuario()
+
+const readAll = async () =>{
+  const querySnapshot = await user.get()
+  const docs = querySnapshot.docs
+  const result = docs.map(doc=>{
+    return{
+      id: doc.id,
+      name: doc.data().name,
+      age: doc.data().age
+    }
+  })
+  console.log(result)
+}
+
+const readById = async () =>{
+  let id = "WaLQZLBQ8lv01FlVn6kV"
+  const doc = user.doc(`${id}`)
+  const _user = await doc.get()
+  const _data = _user.data()
+  console.log(_data)
+}
+
+readById()
